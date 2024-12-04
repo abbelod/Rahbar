2eaeb7000f0ea5d375f662e293c7a63986fb3e61
abbelod <123talhaahmed@gmail.com>
2024-12-04 10:29:56 +0500

    fixed weather

--- myproject/real_time_update/views.py
+++ myproject/real_time_update/views.py
@@ -2,6 +2,12 @@ import requests
 from django.conf import settings
 from django.shortcuts import render
 from urllib.parse import quote
+from .wmo_weather_codes import WMO_WEATHER_CODES
+
+
+
+def get_weather_description(weather_code):
+    return WMO_WEATHER_CODES.get(weather_code, "Unknown weather condition")
 
 def get_geocode(address):
     """
@@ -29,42 +35,22 @@ def get_geocode(address):
 
 
 def get_weather(city):
-    """
-    Fetch weather data for a city using the OpenWeatherMap API.
-    """
-    # city = urllib.parse.urlencode(city)
-    # url = f"https://{settings.OPEN_WEATHER_HOST}/city/{city}/EN"
-    # headers = {
-    #     "x-rapidapi-key": settings.RAPIDAPI_KEY,
-    #     "x-rapidapi-host": settings.OPEN_WEATHER_HOST,
-    # }
-    # response = requests.get(url, headers=headers)
-    # if response.status_code == 200:
-    #     data = response.json()
-    #     if "data" in data:
-    #         return {
-    #             "city": city,
-    #             "temperature": data["data"]["temperature"],
-    #             "description": data["data"]["description"],
-    #         }
-          
-    # return{
-    #              "city": 'lahore',
-    #             "temperature": "15",
-    #             "description": 'Sunny',
-    #         }
-    lat, long = get_lat_lon(city)
+    
+    cord_data = get_lat_lon(city)
+    lat = cord_data['lat']
+    long = cord_data["lon"]
+    city = cord_data["city"]
     url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}8&longitude={long}&current=temperature_2m,weather_code&timezone=auto&forecast_days=1'
     response = requests.get(url)
     if response.status_code == 200:
             data = response.json()
-            if "data" in data:
-                return {
-                    "city": city,
-                    "temperature": data["data"]["current"]["temperature_2m"],
-                    "description": data["data"]['current']["weather_code"],
-                }
-            
+            description = get_weather_description(data['current']["weather_code"])
+            return {
+                "city":city,
+                "temperature": data["current"]["temperature_2m"],
+                "description": description,
+            }
+        
     return{
                  "city": 'lahore',
                 "temperature": "15",
@@ -72,15 +58,12 @@ def get_weather(city):
             }
 
 def real_time_update(request):
-    """
-    Combine geocode and weather data into a single view.
-    """
+
     address = request.GET.get("address", "1600 Amphitheatre Parkway, Mountain View, CA")
     city = request.GET.get("city", "London")
 
     geocode_data = get_geocode(address)
     weather_data = get_weather(city)
-    print(weather_data)
 
     return render(request, "real_time_update/weather_map.html", {
         "geocode": geocode_data,
@@ -89,10 +72,23 @@ def real_time_update(request):
 
 
 def get_lat_lon(city):
+    if not city:
+        city = 'London'
+    
     city = quote(city)
     url = f'https://api.opencagedata.com/geocode/v1/json?q={city}&key={settings.GEOCODING_API}'
     response = requests.get(url)
-    print(response.json())
-    # lat= response.json()['data']['results']['geometry']['lat']
-    # long= response.json()['data']['results']['geometry']['lng']
-    return 1, 2
+    data = response.json()
+    if data.get("results"):  # Check if results exist
+        first_result = data["results"][0]  # Access the first result
+        lat = first_result["geometry"]["lat"]
+        lon = first_result["geometry"]["lng"]
+        normalized_city = first_result['components']['_normalized_city']
+        return {'lat': lat,
+                'lon': lon,
+                'city': normalized_city
+                }
+    else:
+        print("No results found!")
+        return 1, 1
+
